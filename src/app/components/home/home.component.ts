13da98f691072b59f8f7d0fb84be13724f581b83
unknown <johanguerrerochacon@gmail.com>
2025-02-24 22:00:29 -0500

    Subiendo actualizaciones hasta parte cuatro o video 4 del ejercicio

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,57 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-import { Housinglocation } from '../housinglocation';
+import { HousingLocation } from '../housinglocation';
 import { CommonModule } from '@angular/common';
+import { HousingService } from '../housing.service';
 
 @Component({  
   selector: 'app-home',
+  standalone: true,
   imports: [CommonModule, HousingLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-  housinglocationList: Housinglocation [] = [
-    {
-      id: 0,
-      name: 'Acme Fresh Start Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
-      availableUnits: 4,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      id: 1,
-      name: 'A113 Transitional Housing',
-      city: 'Santa Monica',
-      state: 'CA',
-      photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`,
-      availableUnits: 0,
-      wifi: false,
-      laundry: true,
-    },
-    {
-      id: 2,
-      name: 'Warm Beds Housing Support',
-      city: 'Juneau',
-      state: 'AK',
-      photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`,
-      availableUnits: 1,
-      wifi: false,
-      laundry: false,
-    },
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
 
-    {
-      id: 3,
-      name: 'Homesteady Housing',
-       city: 'Chicago',
-      state: 'IL',
-      photo: `${this.baseUrl}/ian-macdonald-W8z6aiwfi1E-unsplash.jpg`,
-      availableUnits: 1,
-      wifi: true,
-      laundry: false,
-      }
-  ];
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
